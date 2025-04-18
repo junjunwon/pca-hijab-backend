142fac357f29992acecde38f4a6dd29fd7375294
junjunwon <45681336+junjunwon@users.noreply.github.com>
2025-04-18 12:51:03 +0900

    feat: 정적 리소스에 대한 HTTP 캐싱 적용
    브라우저에 재요청 X -> 캐시된 이미지 사용

--- src/main/java/com/hijab/config/MvcConfig.java
+++ src/main/java/com/hijab/config/MvcConfig.java
@@ -5,8 +5,12 @@ import com.hijab.common.interceptor.RateLimitInterceptor;
 import com.hijab.common.interceptor.UserIdentifierInterceptor;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
 
 //@EnableWebMvc
 @Configuration
@@ -30,5 +34,12 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addInterceptor(loggerInterceptor);
         registry.addInterceptor(rateLimitInterceptor);
 //        registry.addInterceptor(userIdentifierInterceptor);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS));
     }
 }
